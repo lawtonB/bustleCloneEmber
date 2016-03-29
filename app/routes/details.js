edffbd534fe8c2416261caa61300026d93de0fb8
Lawton Browning and Simon Temple <lawton+simon@>
2016-03-29 14:32:58 -0700

    add individual page details

--- app/routes/details.js
+++ app/routes/details.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord("details", params.details_id);
+    return this.store.findRecord("story", params.details_id);
   }
 });
